924da921c0f9abace1be1424e8ef9b3e71322d97
MihaMarkocic <miha.markocic@gmail.com>
2020-04-06 11:41:42 +0000

    add VM to private subnet

--- AWS/vpc_infrastructure/modules/compute/compute.tf
+++ AWS/vpc_infrastructure/modules/compute/compute.tf
@@ -1,6 +1,6 @@
 # compute.tf deploying ec2 instances placed in already existing vpc (network.tf)
 
-# create ec2 instance
+# deploy webserver in public subnet
 resource "aws_instance" "Webserver-pub" {
 	ami = var.InstanceAmi
 	instance_type = var.InstanceType
@@ -12,5 +12,20 @@ resource "aws_instance" "Webserver-pub" {
 	
 tags = {
 	Name = "Public Webserver"
+}
+}
+
+# deploy webserver in private subnet
+resource "aws_instance" "Webserver-prvt" {
+	ami = var.InstanceAmi
+	instance_type = var.InstanceType
+	subnet_id = var.prvtSubnetID
+	availability_zone = var.ec2AvailabilityZone
+	vpc_security_group_ids = var.securityGroupID
+	associate_public_ip_address = false
+	key_name = var.pubSshKey
+	
+tags = {
+	Name = "Private Webserver"
 }
 }
