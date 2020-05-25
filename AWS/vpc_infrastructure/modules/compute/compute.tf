ef3d62d3be58f44eb2d95bc51449671c4e39ea00
MihaMarkocic <miha.markocic@gmail.com>
2020-05-25 13:13:19 +0000

    tf remote provisioner for apache installation

--- AWS/vpc_infrastructure/modules/compute/compute.tf
+++ AWS/vpc_infrastructure/modules/compute/compute.tf
@@ -1,20 +1,5 @@
 # compute.tf deploying ec2 instances placed in already existing vpc (network.tf)
 
-# deploy webserver in public subnet
-resource "aws_instance" "webserver" {
-	ami = var.instanceAmi
-	instance_type = var.instanceType
-	subnet_id = var.pubSubnetID
-	availability_zone = var.ec2AvailabilityZone
-	vpc_security_group_ids = var.webserverSgId
-	associate_public_ip_address = true
-	key_name = var.pubSshKey
-	
-tags = {
-	Name = "Webserver"
-}
-}
-
 # deploy a jump host
 resource "aws_instance" "jumpHost" {
 	ami = var.instanceAmi
@@ -25,9 +10,41 @@ resource "aws_instance" "jumpHost" {
 	associate_public_ip_address = true
 	key_name = var.pubSshKey
 
-tags = {
-	Name = "JumpHost"
+	tags = {
+		Name = "JumpHost"
+	}
 }
+
+# deploy webserver in public subnet
+resource "aws_instance" "webserver" {
+	ami = var.instanceAmi
+	instance_type = var.instanceType
+	subnet_id = var.pubSubnetID
+	availability_zone = var.ec2AvailabilityZone
+	vpc_security_group_ids = var.webserverSgId
+	associate_public_ip_address = true
+	key_name = var.pubSshKey
+	
+	connection {
+		host = self.private_ip
+		user = "ubuntu"
+		type = "ssh"
+		private_key = file("~/.ssh/id_rsa_webserver")
+		bastion_host = aws_instance.jumpHost.public_ip
+		bastion_private_key = file("~/.ssh/id_rsa_webserver") 
+	}
+
+	provisioner "remote-exec" {
+		inline = [
+			"sudo apt-get -y update",
+			"sudo apt-get -y install apache2",
+			"sudo service apache2 start",
+		]
+	}
+
+	tags = {
+		Name = "Webserver"
+	}
 }
 
 
@@ -41,7 +58,7 @@ resource "aws_instance" "privateVM" {
 	associate_public_ip_address = false
 	key_name = var.pubSshKey
 	
-tags = {
-	Name = "PrivateVM"
-}
+	tags = {
+		Name = "PrivateVM"
+	}
 }
