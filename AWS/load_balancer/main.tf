13eb6137d9ac1bc863cdc9916244b7986d9be3c1
MihaMarkocic <miha.markocic@gmail.com>
2020-11-11 13:24:41 +0000

    output public IPs

--- AWS/load_balancer/main.tf
+++ AWS/load_balancer/main.tf
@@ -24,3 +24,19 @@ module "loadbalancing" {
     web2ID = module.compute.web2ID
     web3ID = module.compute.web3ID
 }
+
+output "webserver1_public_ip" {
+    value = module.compute.web1IP
+}
+
+output "webserver2_public_ip" {
+    value  = module.compute.web2IP
+}
+
+output "webserver3_public_ip" {
+    value= module.compute.web3IP
+}
+
+output "LoadBalancer_public_ip" {
+    value = module.loadbalancing.lbIP
+}
--- AWS/load_balancer/modules/compute/outputs.tf
+++ AWS/load_balancer/modules/compute/outputs.tf
@@ -3,12 +3,27 @@ output "web1ID" {
     value = aws_instance.webserver1.id
 }
 
+output "web1IP" {
+    description = "webserver 1 public IPv4"
+    value = aws_instance.webserver1.public_ip
+}
+
 output "web2ID" {
     description = "webserver2 ID for lb target group attachment"
     value = aws_instance.webserver2.id
 }
 
+output "web2IP" {
+    description = "webserver 2 public IPv4"
+    value = aws_instance.webserver2.public_ip
+}
+
 output "web3ID" {
     description = "webserver2 ID for lb target group attachment"
     value = aws_instance.webserver3.id
+}
+
+output "web3IP" {
+    description = "webserver 3 public IPv4"
+    value = aws_instance.webserver3.public_ip
 }
