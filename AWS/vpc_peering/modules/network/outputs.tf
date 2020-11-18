e51952c264c41c5152a3117cb1dff35c6c25196c
MihaMarkocic <miha.markocic@gmail.com>
2020-11-18 14:54:30 +0000

    added outputs for peering

--- AWS/vpc_peering/modules/network/outputs.tf
+++ AWS/vpc_peering/modules/network/outputs.tf
@@ -15,7 +15,17 @@ output "security_id" {
     value = ["${aws_security_group.instanceSG.id}"]
 }
 
-output "thisVPC_cidr" {
+output "vpc_cidr" {
     description = "this VPC cidr block to include it in the security group rules"
     value = aws_vpc.myVPC.cidr_block
+}
+
+output "vpc_id" {
+    description = "created VPC ID"
+    value = aws_vpc.myVPC.id
+}
+
+output "route_table_id" {
+    description = "route table ID output for peering"
+    value = aws_route_table.myRT.id
 }
