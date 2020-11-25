ad158de693ede3cc3968889cdd16138d51c04165
MihaMarkocic <miha.markocic@gmail.com>
2020-11-25 15:21:47 +0000

    compute not ready

--- AWS/transit_gateway/main.tf
+++ AWS/transit_gateway/main.tf
@@ -6,17 +6,19 @@ variable "region" {
     default = "eu-west-2"
 }
 
+#define the provider
+
 provider "aws" {
     region = var.region
 }
 
 # modules to execute
+
 module "network" {
     source = "./modules/network"
 
     region = var.region
 }
-
 
 module "compute-vpcA" {
     source = "./modules/compute"
