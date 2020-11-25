2e62f4f5bd11769115d60c835bb8bfcc64a47598
MihaMarkocic <miha.markocic@gmail.com>
2020-11-25 16:48:53 +0000

    include instance deployment

--- AWS/transit_gateway/main.tf
+++ AWS/transit_gateway/main.tf
@@ -24,5 +24,16 @@ module "compute-vpcA" {
     source = "./modules/compute"
 
     region = var.region
-    subne1_id = module.network.
+    subnet1_id = module.network.vpcA_subnet1_id
+    subnet2_id = module.network.vpcA_subnet2_id
+    security_id = module.network.vpcA_sg_id
+}
+
+module "compute-vpcB" {
+    source = "./modules/compute"
+
+    region = var.region
+    subnet1_id = module.network.vpcB_subnet1_id
+    subnet2_id = module.network.vpcB_subnet2_id
+    security_id = module.network.vpcB_sg_id
 }
