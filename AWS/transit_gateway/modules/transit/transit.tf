c0e9279e30bdf84b2e7717a82d4fce7691146dbb
MihaMarkocic <miha.markocic@gmail.com>
2020-11-27 17:12:52 +0000

    fixed transit routes, destination and vpc atachment must match!

--- AWS/transit_gateway/modules/transit/transit.tf
+++ AWS/transit_gateway/modules/transit/transit.tf
@@ -42,15 +42,15 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "vpcBtgw" {
 
 #create transit gateway routes
 
-resource "aws_ec2_transit_gateway_route" "transitAtoB" {
+resource "aws_ec2_transit_gateway_route" "routetoB" {
   destination_cidr_block = var.vpcB_cidr
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpcAtgw.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpcBtgw.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw.association_default_route_table_id
 }
 
-resource "aws_ec2_transit_gateway_route" "transitBtoA" {
+resource "aws_ec2_transit_gateway_route" "routetoA" {
   destination_cidr_block = var.vpcA_cidr
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpcBtgw.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.vpcAtgw.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway.tgw.association_default_route_table_id
 }
 
