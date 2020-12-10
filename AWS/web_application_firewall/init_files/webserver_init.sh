03b52fa7f565659931b649d2571f26e0704a0f00
MihaMarkocic <miha.markocic@gmail.com>
2020-12-10 10:56:33 +0000

    fix apache2 module dependency problems

--- AWS/web_application_firewall/init_files/webserver_init.sh
+++ AWS/web_application_firewall/init_files/webserver_init.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
 #install apache
 sudo apt-get --assume-yes update
+sudo apt-get -f autoremove
+sudo apt-get --assume-yes update
 sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes dist-upgrade
 sudo apt-get --assume-yes install apache2
 
 # Apply FW rules to accept connections on 22, 80 and 443
